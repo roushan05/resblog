f57b22f2a6defca351cdb22a98826ead7f201349
Roushan Kumar <roushan.101004@gmail.com>
2017-02-07 01:12:29 +0530

    first commit

---
+++ posts/models.py
@@ -0,0 +1,19 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length = 250)
+    pub_date = models.DateTimeField()
+    image = models.ImageField(upload_to = 'media/')
+    body = models.TextField()
+
+
+    def __str__(self):
+        return self.title
+
+    def date_fr(self):
+        return self.pub_date.strftime('%b %d %Y')
+    def summary(self) :
+        return self.body[:100]
